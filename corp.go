ddc69ca0c36e8c732259860a5bb82fe43c7d8aac
Nick Mueller <nick.mueller.at@gmail.com>
2017-01-25 14:13:29 +0100

    Implemented CorpStarbaseList and CorpStarbaseDetails endpoints

--- corp.go
+++ corp.go
@@ -6,9 +6,11 @@ import (
 )
 
 const (
-	CorpContactListURL    = "/corp/ContactList.xml.aspx"
-	CorpAccountBalanceURL = "/corp/AccountBalance.xml.aspx"
-	CorpWalletJournalURL  = "/corp/WalletJournal.xml.aspx"
+	CorpContactListURL     = "/corp/ContactList.xml.aspx"
+	CorpAccountBalanceURL  = "/corp/AccountBalance.xml.aspx"
+	CorpStarbaseListURL    = "/corp/StarbaseList.xml.aspx"
+	CorpStarbaseDetailsURL = "/corp/StarbaseDetail.xml.aspx"
+	CorpWalletJournalURL   = "/corp/WalletJournal.xml.aspx"
 )
 
 type Contact struct {
@@ -65,6 +67,112 @@ type AccountBalance struct {
 func (api API) CorpAccountBalances() (*AccountBalance, error) {
 	output := AccountBalance{}
 	err := api.Call(CorpAccountBalanceURL, nil, &output)
+	if err != nil {
+		return nil, err
+	}
+	if output.Error != nil {
+		return nil, output.Error
+	}
+	return &output, nil
+}
+
+type StarbaseList struct {
+	APIResult
+	Starbases []*Starbase `xml:"result>rowset>row"`
+}
+
+type Starbase struct {
+	ID              int           `xml:"itemID,attr"`
+	TypeID          int           `xml:"typeID,attr"`
+	LocationID      int           `xml:"locationID,attr"`
+	MoonID          int           `xml:"moonID,attr"`
+	State           StarbaseState `xml:"state,attr"`
+	StateTimestamp  eveTime       `xml:"stateTimestamp,attr"`
+	OnlineTimestamp eveTime       `xml:"onlineTimestamp,attr"`
+	StandingOwnerID int           `xml:"standingOwnerID,attr"`
+}
+
+type StarbaseState int
+
+const (
+	StarbaseStateUnanchored StarbaseState = 0
+	StarbaseStateAnchored   StarbaseState = 1
+	StarbaseStateOnlining   StarbaseState = 2
+	StarbaseStateReinforced StarbaseState = 3
+	StarbaseStateOnline     StarbaseState = 4
+)
+
+func (s StarbaseState) String() string {
+	switch s {
+	case StarbaseStateUnanchored:
+		return "unanchored"
+	case StarbaseStateAnchored:
+		return "anchored/offline"
+	case StarbaseStateOnlining:
+		return "onlining"
+	case StarbaseStateReinforced:
+		return "reinforced"
+	case StarbaseStateOnline:
+		return "online"
+	default:
+		return "unknown/invalid"
+	}
+}
+
+func (api API) CorpStarbaseList() (*StarbaseList, error) {
+	output := StarbaseList{}
+	err := api.Call(CorpStarbaseListURL, nil, &output)
+	if err != nil {
+		return nil, err
+	}
+	if output.Error != nil {
+		return nil, output.Error
+	}
+	return &output, nil
+}
+
+type StarbaseDetails struct {
+	APIResult
+	State           StarbaseState `xml:"result>state"`
+	StateTimestamp  eveTime       `xml:"result>stateTimestamp"`
+	OnlineTimestamp eveTime       `xml:"result>onlineTimestamp"`
+	GeneralSettings struct {
+		UsageFlags              int  `xml:"usageFlags"`
+		DeployFlags             int  `xml:"deployFlags"`
+		AllowCorporationMembers bool `xml:"allowCorporationMembers"`
+		AllowAllianceMembers    bool `xml:"allowAllianceMembers"`
+	} `xml:"result>generalSettings"`
+	CombatSettings struct {
+		UseStandingsFrom struct {
+			OwnerID int `xml:"ownerID,attr"`
+		} `xml:"useStandingsFrom"`
+		OnStandingDrop struct {
+			Standing int `xml:"standing,attr"`
+		} `xml:"onStandingDrop"`
+		OnStatusDrop struct {
+			Enabled  bool `xml:"enabled,attr"`
+			Standing int  `xml:"standing,attr"`
+		} `xml:"onStatusDrop"`
+		OnAgression struct {
+			Enabled bool `xml:"enabled,attr"`
+		} `xml:"onAgression"`
+		OnCorporationWar struct {
+			Enabled bool `xml:"enabled, attr"`
+		} `xml:"onCorporationWar"`
+	} `xml:"result>combatSettings"`
+	Fuel []StarbaseFuel `xml:"result>rowset>row"`
+}
+
+type StarbaseFuel struct {
+	TypeID   int `xml:"typeID,attr"`
+	Quantity int `xml:"quantity,attr"`
+}
+
+func (api API) CorpStarbaseDetails(starbaseID int) (*StarbaseDetails, error) {
+	output := StarbaseDetails{}
+	args := url.Values{}
+	args.Set("itemID", strconv.Itoa(starbaseID))
+	err := api.Call(CorpStarbaseDetailsURL, args, &output)
 	if err != nil {
 		return nil, err
 	}
